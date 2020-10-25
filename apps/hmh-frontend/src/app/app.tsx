29666d835f870a4a915394ca6daab699449dcbfc
CalvineHicks <calvinehicks@gmail.com>
2020-10-25 14:28:18 -0700

    add react bootstrap

--- apps/hmh-frontend/src/app/app.tsx
+++ apps/hmh-frontend/src/app/app.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-
 import './app.scss';
-
 import { ReactComponent as Logo } from './logo.svg';
 import {
   BrowserRouter as Router,
@@ -9,48 +7,50 @@ import {
   Route,
   Link
 } from 'react-router-dom';
+import { Col, Container, Row } from 'react-bootstrap';
 export const App = () => {
-  /*
-   * Replace the elements below with your own.
-   *
-   * Note: The corresponding styles are in the ./app.scss file.
-   */
   //TODO: add logo to header
   return (
     <div className='app'>
       <Router>
-      <header className='flex'>
-        <Logo width='75' height='75' />
-        <h1>Heal My Hearing</h1>
-          <nav>
-            <ul>
-              <li>
-                <Link to='/'>Home</Link>
-              </li>
-              <li>
-                <Link to='/about'>About</Link>
-              </li>
-              <li>
-                <Link to='/users'>Users</Link>
-              </li>
-            </ul>
-          </nav>
-      </header>
-      <main>
-          <div>
-            <Switch>
-              <Route path='/about'>
-                <div>about</div>
-              </Route>
-              <Route path='/users'>
-                <div>user</div>
-              </Route>
-              <Route path='/'>
-                <div>home</div>
-              </Route>
-            </Switch>
-          </div>
-      </main>
+        <header className='flex'>
+          <Container>
+            <nav>
+              <Row>
+                <Logo width='75' height='75' />
+                <Col sm='4'>
+                  <h1>Heal My Hearing</h1>
+                </Col>
+                  <Col sm='2'>
+                    <Link to='/'>Home</Link>
+                  </Col>
+                  <Col>
+                      <Link to='/about'>About</Link>
+                  </Col>
+                  <Col>
+                    <Link to='/users'>Users</Link>
+                  </Col>
+              </Row>
+            </nav>
+          </Container>
+        </header>
+        <main>
+          <Container>
+            <div>
+              <Switch>
+                <Route path='/about'>
+                  <div>about</div>
+                </Route>
+                <Route path='/users'>
+                  <div>user</div>
+                </Route>
+                <Route path='/'>
+                  <div>home</div>
+                </Route>
+              </Switch>
+            </div>
+          </Container>
+        </main>
       </Router>
     </div>
   );
